2e9ad1f16a7439c6a0dd78b2d64eabad9a9dd490
Vic Chimenti <vicchimenti@users.noreply.github.com>
2020-01-31 09:07:45 -0800

    Update courses-type.js

--- courses-type.js
+++ courses-type.js
@@ -11,35 +11,39 @@
 */
 
 
-try {
-    /* -- Initialize function variables -- */
-    var groupTitle = content.get("Name");
-    var groupNumber = content.get("Course Number");
-    var groupDescription = content.get("Group Description");
-    var courseSection = content.get("Section");
-    var courseTitle = content.get("Course Title");
-    var courseType = content.get("Course Type");
-    var term = content.get("Term");
-    var year = content.get("Year");
-    var faculty = content.get("Faculty");
-    var courseSummary = content.get("Course Summary");
-    var courseDescription = content.get("Course Description");
-    var prerequisites = content.get("Prerequisites");
-    var keywords = content.get("Keywords");
+    try {
+        /* -- Initialize function variables -- */
+        var groupTitle = content.get("Name");
+        var groupNumber = content.get("Course Number");
+        var groupDescription = content.get("Group Description");
+        var courseSection = content.get("Section");
+        var courseTitle = content.get("Course Title");
+        var courseType = content.get("Course Type");
+        var term = content.get("Term");
+        var year = content.get("Year");
+        var faculty = content.get("Faculty");
+        var courseSummary = content.get("Course Summary");
+        var courseDescription = content.get("Course Description");
+        var prerequisites = content.get("Prerequisites");
+        var keywords = content.get("Keywords");
+
+        /* -- Error Checking -- */
+    }   catch(err) {
+            document.write(err.message);
+    }
 
 
-
-  var programImageMedia = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Education Abroad Program Image' output='normal' formatter='path/*' />");
-  var altImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Alt Image' output='normal' modifiers='striptags,htmlentities' />");
-  var city = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='City' output='normal' display_field='value' />");
-  var generalDescription = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='General Description' output='normal' display_field='value' />");
-  var externalWebsiteLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Website Link' output='normal' display_field='value' />");
-  var programType = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Program Type' output='normal' display_field='value' />");
-  var listOfTypes = "";
-  var term = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Term' output='normal' display_field='value' />");
-  var listOfTerms = "";
-  var prerequisites = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Prerequisites' output='normal' display_field='value' />");
-  var keyWords = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Keywords' output='normal' display_field='value' />");
+  // var programImageMedia = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Education Abroad Program Image' output='normal' formatter='path/*' />");
+  // var altImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Alt Image' output='normal' modifiers='striptags,htmlentities' />");
+  // var city = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='City' output='normal' display_field='value' />");
+  // var generalDescription = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='General Description' output='normal' display_field='value' />");
+  // var externalWebsiteLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Website Link' output='normal' display_field='value' />");
+  // var programType = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Program Type' output='normal' display_field='value' />");
+  // var listOfTypes = "";
+  // var term = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Term' output='normal' display_field='value' />");
+  // var listOfTerms = "";
+  // var prerequisites = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Prerequisites' output='normal' display_field='value' />");
+  // var keyWords = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Keywords' output='normal' display_field='value' />");
 
 
   /* -- Prepare all the things -- */
