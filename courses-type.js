5086e697d4327a79d4d05a25f3e2f16c62ebee6f
Vic Chimenti <vicchimenti@users.noreply.github.com>
2020-02-19 08:48:23 -0800

    rm bracket

--- courses-type.js
+++ courses-type.js
@@ -76,7 +76,7 @@ try {
 
   /* -- Write the collapsed viewable summary header -- */
   document.write('<div class="col-xs-12 fullcourseDescriptionWrapper">');
-  document.write('<div class="row col-xs-12 properCourseNames"><h5>Proper Course Name: </h5><span class="properCourseName">' + properCourseName + '</span>></div>');
+  document.write('<div class="row col-xs-12 properCourseNames"><h5>Proper Course Name: </h5><span class="properCourseName">' + properCourseName + '</span></div>');
   document.write('<div class="row col-xs-12 courseDescriptions"><h5>Course Description: </h5><span class="courseDescription">' + courseDescription + '</span></div></div>');
 
   /* -- Write Program Level 1 Details --*/
