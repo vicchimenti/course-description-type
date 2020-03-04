4b9f3d18466bda112d6851115efbb97c67acee84
Vic Chimenti <vicchimenti@users.noreply.github.com>
2020-03-04 11:14:12 -0800

    ud

--- filter-search.js
+++ filter-search.js
@@ -15,35 +15,39 @@
 
 <script>
 
-//** global array holds list of content items that will render after filter selction **//
-var visibleItems = [];
 
 
 
 
-//** parse out unselected content items and limit display to user selected items **/
- $(function parseItems() {
-    // assign array of currently visible content items
-    visibleItems = $('.courseItemWrapper').not('.hideByText, .hideByType, .hideByTerm, .hideByModule, .hideByCommon, .hideByFaculty');
-    // check to see if array is empty
-    if (visibleItems.length == 0) {
-        // when array is empty show the results message
-        $('.noResultsToShow').removeClass('hideResultsMessage');
-    } else {
-        // when array has content items suppress the results message
-        $('.noResultsToShow').addClass('hideResultsMessage');
-    }
-});
 
-
-
-
-//** wait for window to load then set timeout before reacting to filter search input **/
 $(function () {
     // After the DOM is ready, Wait until the window loads
     $(window).load(function () {
         // Once window loads set a timeout delay
         setTimeout(function () {
+
+
+
+
+            //** global array holds list of content items that will render after filter selction **//
+            var visibleItems = [];
+
+
+
+
+            //** parse out unselected content items and limit display to user selected items **/
+            $(function parseItems() {
+                // assign array of currently visible content items
+                visibleItems = $('.courseItemWrapper').not('.hideByText, .hideByType, .hideByTerm, .hideByModule, .hideByCommon, .hideByFaculty');
+                // check to see if array is empty
+                if (visibleItems.length == 0) {
+                    // when array is empty show the results message
+                    $('.noResultsToShow').removeClass('hideResultsMessage');
+                } else {
+                    // when array has content items suppress the results message
+                    $('.noResultsToShow').addClass('hideResultsMessage');
+                }
+            });
             
             
             
